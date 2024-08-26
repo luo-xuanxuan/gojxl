2813034709d1e0c6389ed75d1f28598e3fa5a69f
luo-xuanxuan <luoisaxuanxuan@gmail.com>
2024-08-26 14:13:34 -0700

    implement jpegComponent

--- formats/jxl.go
+++ formats/jxl.go
@@ -1,1 +1,28 @@
 package formats
+
+type coeff int16
+
+type jpegComponent struct {
+	//four byte component id, might only need 1 byte?
+	id uint32
+	// Horizontal and vertical sampling factors.
+	// In interleaved mode, each minimal coded unit (MCU) has
+	// hSampleFactor x vSampleFactor DCT blocks from this component.
+	hSampleFactor int64
+	vSampleFactor int64
+	// The index of the quantization table used for this component.
+	quantizationIndex uint32
+	// The dimensions of the component measured in 8x8 blocks.
+	widthInBlocks  uint32
+	heightInBlocks uint32
+	// The DCT coefficients of this component, laid out block-by-block, divided
+	// through the quantization matrix values.
+	coeffs []coeff
+}
+
+func newJPEGComponent() jpegComponent {
+	return jpegComponent{
+		hSampleFactor: 1,
+		vSampleFactor: 1,
+	}
+}
